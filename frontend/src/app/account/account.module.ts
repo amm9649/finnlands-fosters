93347ee62bca0c23f4ce991b3ba65a99fb20c31e
Amy Mays <amymays9649@gmail.com>
2022-11-02 00:38:45 -0400

    ...

---
+++ frontend/src/app/account/account.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AccountRoutingModule } from './account-routing.module';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AccountRoutingModule,
+  ],
+})
+export class AccountModule { }
---
+++ frontend/src/app/account/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '@app/_services';
+
+@Component({ templateUrl: './login.component.html' })
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  // convenience getter
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.form.invalid){
+      return;
+    }
+
+    this.loading = true;
+    this.accountService.login(this.f['username'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          console.log(this.f);
+          this.router.navigateByUrl('/account/profile');
+        },
+        error: error => {
+          console.log(error);
+          this.loading = false;
+        }
+      })
+  }
+
+}
